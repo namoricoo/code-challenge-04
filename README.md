ad0c8a0c14133d9be3a6b11465b999f1c653fb8a
Nate <namoricoo@hotmail.com>
2013-11-25 14:03:23 -0800

    Pass Test for reading a file into an Array

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 1)Initial Commit Write Test for reading a file into an Array
 
 2)Zero Rubocop offenses for load_tab_file_spec.rb
+
+3)Pass Test for reading a file into an Array
+
+4)Zero Rubocop offenses for load_tab_file.rb
--- lib/load_tab_file.rb
+++ lib/load_tab_file.rb
@@ -1,4 +1,19 @@
 # load tab separated values into an array.
 class LoadTabFile
-  
+  def initialize(file_name)
+    file_relative_path = File.dirname(__FILE__)
+    @file_relative_path_and_name = "#{file_relative_path}/#{file_name}"
+  end
+
+  def read_file_into_array
+    input_array = []
+    File.open(@file_relative_path_and_name, 'r') do |file|
+      loop do
+        data_in_line = file.gets
+        break unless data_in_line == nil?
+        input_array << data_in_line
+      end
+    end
+    input_array
+  end
 end
